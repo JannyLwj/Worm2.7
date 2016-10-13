9806a2afeb53caf9debf8e35f4dbff14c9c88d38
Janny <187231153@qq.com>
2016-10-13 14:36:42 +0800

    Version5

--- SpiderBBS.py
+++ SpiderBBS.py
@@ -8,8 +8,7 @@ class BBS:
     def __init__(self):
         self.user_agent= 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
         self.headers = {'User-Agent': self.user_agent}
-        self.stories=[]
-        self.enable=False
+        BBSstories=[]
 
     def getPage(self):
         try:
@@ -24,56 +23,23 @@ class BBS:
             if hasattr(e, "reason"):
                 print e.reason
 
-    def getPageItems(self):
-        pageContent=self.getPage()
-        if not pageContent:
-            print "页面加载失败"
-            return None
-        pattern = re.compile('<tr.*? valign=bottom.*?>.*?<td.*?style.*?<a.*?<span.*?>(.*?)'
-                             '</span>.*?<td.*?</td>.*?<td.*?>.*?<span.*?>(.*?)'
-                             '</span>.*?<td.*?>.*?<span.*?>(.*?)'
-                             '</span>.*?<td.*?>.*?<span.*?>(.*?)'
-                             '</span>.*?<td.*?>.*?<span.*?>(.*?)'
-                             '</span>.*?<td.*?>.*?<span.*?>(.*?)'
-                             '</span>',re.S)
-        items = re.findall(pattern, pageContent)
-        pageStories=[]
+    def getBBScontent(self, BBScontent):
+        pattern = re.compile('<tr valign="bottom".*?>.*?</tr>', re.S)
+        items = re.findall(pattern, BBScontent)
         for item in items:
-            print item[0]
-            print item[1]
-            print item[2]
-            print item[3]
-            print item[4]
-            print item[5]
-            pageStories.append([item[0].strip(),item[1].strip(),item[2].strip(),item[3].strip(),item[4].strip(),item[5].strip()])
-        return pageStories
-
-    def loadPage(self):
-        if self.enable==True:
-            if len(self.stories)<2:
-                pageStories=self.getPageItems()
-                if pageStories:
-                    self.stories.append(pageStories)
-
-
-    def getOneStory(self, pageStories):
-        for story in pageStories:
-            input=raw_input()
-            self.loadPage()
-            if input=="Q":
-                self.enable=False
-                return
-            print u"贴主题:%s\t回复人数:%s\t浏览人数:%s\t发帖人:%s\t发布时间:%s\t更新时间:%s\n" %(story[0],story[1],story[2],story[3],story[4],story[5])
+            if "Label1" in item and  "Label2" in item and  "Label6" in item:
+                BBStitle=re.search('(?<=Label1\">).+(?=</span>)', item)
+                BBSresponse=re.search('(?<=Label2\">).+(?=</span>)', item)
+                BBStime = re.search('(?<=Label7\">).+(?=</span>)', item)
+                print BBStitle.group()
+                print BBSresponse.group()
+                print BBStime.group()
+                #存入数据库
 
     def start(self):
-        print u"正在BBS帖子，按回车查看新段子，Q退出"
-        self.enable = True
-        self.loadPage()
-        while self.enable:
-            if len(self.stories) > 0:
-                pageStories=self.stories[0]
-                del self.stories[0]
-                self.getOneStory(pageStories)
+        content=self.getPage()
+        self.getBBScontent(content)
+
 
 spider=BBS()
 spider.start()
